8227248280e39d157150e4b2071642303c3e3b71
CoNoYaR0 <152749633+CoNoYaR0@users.noreply.github.com>
2025-07-21 15:44:31 +0200

    Update 007_fix_incorrect_image_urls.sql

--- migrations/007_fix_incorrect_image_urls.sql
+++ migrations/007_fix_incorrect_image_urls.sql
@@ -4,11 +4,20 @@
 BEGIN;
 
 UPDATE product_images
-SET cdn_url = 'https://cdn.stainedglass.tn/' || REPLACE(original_dolibarr_path, '/home/stainea/documents/produit/', '') || '/' || original_dolibarr_filename
+SET cdn_url = 'https://cdn.stainedglass.tn/' ||
+              REPLACE(original_dolibarr_path, '/home/stainea/documents/produit/', '') || '/' ||
+              original_dolibarr_filename
 WHERE original_dolibarr_path IS NOT NULL
   AND original_dolibarr_path != ''
   AND original_dolibarr_filename IS NOT NULL
   AND original_dolibarr_filename != '';
+
+-- Add subfolder based on product name
+UPDATE product_images
+SET cdn_url = 'https://cdn.stainedglass.tn/' ||
+              SPLIT_PART(original_dolibarr_filename, '-', 1) || '/' ||
+              original_dolibarr_filename
+WHERE cdn_url LIKE 'https://cdn.stainedglass.tn/%';
 
 -- Then, remove any double slashes
 UPDATE product_images
